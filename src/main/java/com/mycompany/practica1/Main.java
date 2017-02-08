4641ff022906b0017c15c59652abdf133031928f
HectorQuinonesBarrios <a291528@uach.mx>
2017-02-08 05:29:20 -0700

    Subida
    Se subio la practica

---
+++ src/main/java/com/mycompany/practica1/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.practica1;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
